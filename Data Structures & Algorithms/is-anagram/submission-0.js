c9f09af29f6ec07ef043eeb7e4c937867130af46
susmito96 <susmito.learn@gmail.com>
2026-03-30 21:30:02 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+        const countMap = {
+            [s]: {},
+            [t]: {}
+        };
+
+        for (let i = 0; i < s.length; i++) {
+            if (countMap[s].hasOwnProperty(s[i])) {
+                countMap[s][s[i]] += 1;
+            } else {
+                countMap[s][s[i]] = 1;
+            }
+            if (countMap[t].hasOwnProperty(t[i])) {
+                countMap[t][t[i]] += 1;
+            } else {
+                countMap[t][t[i]] = 1;
+            }
+        }
+
+        if (Object.keys(countMap[s]).length !== Object.keys(countMap[t]).length) {
+            return false;
+        }
+
+        for (let k in countMap[s]) {
+            if (countMap[s][k] !== countMap[t][k]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
